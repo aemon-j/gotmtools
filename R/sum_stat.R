01fe3ff3339347a40b20900e0e34d4a181088e0d
tadhg.m <tadhg.moore@dkit.ie>
2020-01-03 14:49:11 +0000

    Added calculation of log-likelihood (beta)

--- R/sum_stat.R
+++ R/sum_stat.R
@@ -1,16 +1,16 @@
 #' Caclulate summary stats of mod v obs
 #'
-#' Calculate general summary statistics of modelled water temperature vs observed water temperature; Pearson's R, variance, covariance, bias, Nash-Sutcliffe Efficiency (NSE) and Root mean squared error (RMSE).
+#' Calculate general summary statistics of modelled water temperature vs observed water temperature; Pearson's R, variance, covariance, bias, Nash-Sutcliffe Efficiency (NSE) and Root mean squared error (RMSE) and log-likelihood (lnlikelihood).
 #'
 #' @param mod vector or dataframe; Vector if no depth values otherwise Modelled values in the long format. i.e. same as observed in `parsac`
-#' @param obs vector or dataframe; Vector if no depth values otherwise Observed values in the long format loaded in using load.obs
+#' @param obs vector or dataframe; Vector if no depth values otherwise Observed values in the long format used in `parsac`
 #' @param depth logical; Depth values are included. Defaults to False
 #' @param na.rm logical; Remove NA'values
 #' @param depth.range vector; vector with a depth range to extract statistics at certain depths. Upper limit first e.g. c(-5,-10). Defaults to whole depth range.
 #' @return data frame of summary statistics
 #' @import stats
 #' @export
-sum_stat <- function(mod, obs, depth =F,na.rm =T, depth.range =NULL){
+sum_stat <- function(mod, obs, depth = FALSE, na.rm = TRUE, depth.range = NULL){
   if(depth == T){
     if(!is.null(depth.range)){
       obs = obs[(obs[,2] <= depth.range[1] & obs[,2] >= depth.range[2]),]
@@ -30,10 +30,11 @@ sum_stat <- function(mod, obs, depth =F,na.rm =T, depth.range =NULL){
     mae = mean(abs(dif), na.rm = na.rm)
     rmse = sqrt(mean(dif^2, na.rm = na.rm))
     nse = NSE(df$mod, df$obs, na.rm = na.rm)
+    lnlikelihood = sum(dnorm(df$obs, mean = df$mod, log = TRUE), na.rm = na.rm)
     summary_stats = data.frame(Pearson_r = pear_r$estimate,Variance_obs = var_obs,
                                Variance_mod = var_mod, SD_obs = SD_obs, SD_mod = SD_mod,
                                Covariance = cov, #Correlation =cor,
-                               Bias = bias, MAE = mae, RMSE = rmse, NSE = nse, row.names = c())
+                               Bias = bias, MAE = mae, RMSE = rmse, NSE = nse, lnlikelihood = lnlikelihood, row.names = c())
     return(summary_stats)
   }else{
     dif = mod- obs
@@ -48,10 +49,11 @@ sum_stat <- function(mod, obs, depth =F,na.rm =T, depth.range =NULL){
     mae = mean(abs(dif), na.rm = na.rm)
     rmse = sqrt(mean(dif^2, na.rm = na.rm))
     nse = NSE(mod, obs, na.rm = na.rm)
+    lnlikelihood = sum(dnorm(obs, mean = mod, log = TRUE), na.rm = na.rm)
     summary_stats = data.frame(Pearson_r = pear_r$estimate,Variance_obs = var_obs,
                                Variance_mod = var_mod, SD_obs = SD_obs, SD_mod = SD_mod,
                                Covariance = cov, #Correlation =cor,
-                               Bias = bias, MAE = mae, RMSE = rmse, NSE = nse, row.names = c())
+                               Bias = bias, MAE = mae, RMSE = rmse, NSE = nse, lnlikelihood = lnlikelihood, row.names = c())
     return(summary_stats)
   }
 
