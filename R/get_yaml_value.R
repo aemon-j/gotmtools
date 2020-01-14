e6a85ca125bdab3a2eaae1d92c5ea646fb58a5b2
JFeldbauer <johannes.feldbauer@posteo.de>
2020-01-14 11:12:10 +0100

    added ability to read lists to get_yaml_value()

--- R/get_yaml_value.R
+++ R/get_yaml_value.R
@@ -13,7 +13,7 @@
 #' yaml_file <- system.file('extdata/gotm.yaml', package = 'GOTMr')
 #' get_yaml_value(file = yaml_file, label = 'airp', key = 'file')
 #' @export
-#' 
+#'
 get_yaml_value <- function(file = 'gotm.yaml', label, key){
   yml <- readLines(file)
 
@@ -40,6 +40,8 @@ get_yaml_value <- function(file = 'gotm.yaml', label, key){
   #Split to extract comment
   spl1 <- strsplit(yml[ind_map], c('#'))[[1]]
   spl2 <- strsplit(spl1[1], ': ')[[1]][2]
+
+
   if(grepl(':', spl2)){
     spl3 = strsplit(spl2, ' ')[[1]]
     val = paste(spl3[1], spl3[2])
@@ -47,23 +49,55 @@ get_yaml_value <- function(file = 'gotm.yaml', label, key){
     val <- gsub(" ", "", spl2, fixed = TRUE)
   }
 
-  val2 <- NULL
+  # check if item is a list
+  if(length(grep("  - ",yml[ind_map+1]))>0){
+    lst <- list(yml[ind_map + 1])
+    k <- 2
+    while (length(grep("  - ",yml[ind_map + k]))>0) {
+      lst[[k]] <- yml[ind_map + k]
+      k <- k+1
+    }
+    val <- unlist(lapply(lst,function(x){strsplit(x,"- ")[[1]][2]}))
+  }
 
-  if(val == 'false'){
-    val2 = FALSE
+  if (length(val)==1){
+    val2 <- NULL
+
+    if(val == 'false'){
+      val2 = FALSE
+    }
+    if(val == 'true'){
+      val2 = TRUE
+    }
+    flg <- TRUE
+    flg <- tryCatch({!is.na(as.numeric(val))},
+                    warning = function(x)return(FALSE))
+    if(flg){
+      val2 = as.numeric(val)
+    }
+    if(is.null(val2)){
+      val2 <- gsub('\"',"", val)
+      val2 <- as.character(val2)
+    }
+  } else {
+    val2 <- NULL
+
+    if(all(val %in% c('false','true'))){
+      val2 <- rep(TRUE,length(val))
+      val2[val %in% 'false'] = FALSE
+    }
+
+    flg <- TRUE
+    flg <- tryCatch({!is.na(as.numeric(val))},
+                    warning = function(x)return(FALSE))
+    if(all(flg)){
+      val2 = as.numeric(val)
+    }
+    if(is.null(val2)){
+      val2 <- gsub('\"',"", val)
+      val2 <- as.character(val2)
+    }
   }
-  if(val == 'true'){
-    val2 = TRUE
-  }
-  flg <- TRUE
-  flg <- tryCatch({!is.na(as.numeric(val))},
-                  warning = function(x)return(FALSE))
-  if(flg){
-    val2 = as.numeric(val)
-  }
-  if(is.null(val2)){
-    val2 <- gsub('\"',"", val)
-    val2 <- as.character(val2)
-  }
+
   return(val2)
 }
