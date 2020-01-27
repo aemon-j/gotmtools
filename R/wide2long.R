39de3234f1be259afc3271f824b8e2ac49c3e2bc
tadhg.m <tadhg.moore@dkit.ie>
2020-01-27 09:17:30 +0000

    Works with depths as a vector

--- R/wide2long.R
+++ R/wide2long.R
@@ -2,8 +2,8 @@
 #'
 #' Convert a dataframe in wide format to long format.
 #'
-#' @param data dataframe; values loaded with load.3d
-#' @param depths dataframe; depths loaded with load.depths
+#' @param data dataframe; values loaded with get_vari()
+#' @param depths dataframe; depths loaded with get_vari() or vector of depths corresponding to values in the datafra
 #' @return dataframe in the same format as the observation file
 #' @examples
 #' sim_folder <- system.file('extdata', package = 'GOTMr')
@@ -21,11 +21,20 @@ wide2long <- function(data, depths){
   tmp = c()
   pb = txtProgressBar(min = 0, max = nrow(data), style = 3)
   nc = 2:ncol(data)
-  for(i in 1:nrow(data)){
-    deps = append(deps, as.numeric(depths[i,nc]))
-    tmp = append(tmp, as.numeric(data[i,nc]))
-    setTxtProgressBar(pb, i)
+  if(length(dim(data)) == length(dim(depths))){
+    for(i in 1:nrow(data)){
+      deps = append(deps, as.numeric(depths[i,nc]))
+      tmp = append(tmp, as.numeric(data[i,nc]))
+      setTxtProgressBar(pb, i)
+    }
+  }else{
+    for(i in 1:nrow(data)){
+      deps = append(deps, as.numeric(depths))
+      tmp = append(tmp, as.numeric(data[i,nc]))
+      setTxtProgressBar(pb, i)
+    }
   }
+
   close(pb)
   df = data.frame(date = tim, depths = deps, temp = tmp)
   return(df)
