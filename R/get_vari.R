c2a47af93607b219bf5e7c2639222edf6843bf1b
tadhg.m <tadhg.moore@dkit.ie>
2020-09-22 10:09:17 -0400

    If coordinates are not in ncdf use print = F

--- R/get_vari.R
+++ R/get_vari.R
@@ -6,6 +6,7 @@
 #' @param var character; Name of the variable to be extracted. Must match short name in netCDF file
 #' @param incl_time boolean; Add time to the first column in the dataframe. Defaults to TRUE
 #' @param print logical; Print the name and units of the variable extracted, defaults to TRUE
+#' If coordinates are not in ncdf use print = FALSE
 #' @return dataframe in the same format as the observation file with the surface in the top column and the bottom in the last column.
 #' @importFrom ncdf4 nc_open
 #' @importFrom ncdf4 nc_close
@@ -59,10 +60,10 @@ get_vari <- function(ncdf, var, incl_time = TRUE, print = TRUE){
       var1 <- data.frame(time, var1)
     }
   }
-  mat = matrix(data = c(var, lnam, tunits$units, tunits$coordinates),
-               dimnames = list(c("short_name", "long_name",
-                                 "units", "dimensions"), c()))
   if (print == TRUE) {
+    mat = matrix(data = c(var, lnam, tunits$units, tunits$coordinates),
+                 dimnames = list(c("short_name", "long_name",
+                                   "units", "dimensions"), c()))
     message("Extracted ", var, " from ", ncdf)
     print(mat)
   }
