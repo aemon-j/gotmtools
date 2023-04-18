e22ca2b63291604061132680f61ad43fd8f2b845
Tadhg Moore <tadhg.moore6@gmail.com>
2023-04-18 09:04:57 -0400

    Now replaces tilde '~' with empty space ''

--- R/write_yaml.R
+++ R/write_yaml.R
@@ -12,21 +12,22 @@
 #'
 #' \dontrun{
 #' config_file <- system.file("extdata/feeagh/LakeEnsemblR.yaml", package = "LakeEnsemblR")
-#' 
+#'
 #' yaml <- read_yaml(config_file)
 #' yaml <- set_yaml(yaml, value = 23, key1 = "location", key2 = "latitude")
 #' yaml <- set_yaml(yaml, value = "2010-06-01 00:00:00", key1 = "time", key2 = "start")
 #' yaml <- set_yaml(yaml, value = "meteo.csv", key1 = "input", key2 = "meteo", key3 = "file")
 #' yaml <- set_yaml(yaml, value = TRUE, key1 = "calibration", key2 = "GOTM", key3 = "turb_param/k_min", key4 = "log")
 #' yaml <- set_yaml(yaml, value = c("temp", "salt"), key1 = "output", key2 = "variables")
-#' 
+#'
 #' write_yaml(yaml, "LakeEnsemblR.yaml")
 #' }
- 
+
 write_yaml <- function(x, file, fileEncoding = "UTF-8", ...) {
   yaml::write_yaml(x, file, fileEncoding, ...)
   lins <- readLines(file)
   lins <- gsub("\\byes", "true", lins)
   lins <- gsub("\\bno", "false", lins)
+  lins <- gsub("~", "", lins)
   writeLines(lins, file)
 }
